823d1ac0556f4c36649970ed97bb30d3ac2615e9
S1N0X <antekbal06@gmail.com>
2025-04-10 19:38:27 +0200

    Deleted animation

--- script.js
+++ script.js
@@ -5,14 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
         const section = btn.closest('section');
         const infoList = section.querySelector('.infoList');
 
-        if (!infoList.classList.contains('show')) {
-            section.classList.add('collapsed');
-        }
-
         btn.addEventListener('click', () => {
             infoList.classList.toggle('show');
             btn.classList.toggle('rotate');
-            section.classList.toggle('collapsed', !infoList.classList.contains('show'));
         });
     });
 });
